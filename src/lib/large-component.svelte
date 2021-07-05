b45cb7414a53f23f1aa5fe6b7991e4f9201d47f1
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-07-05 11:45:52 -0700

    minimum reproduction

---
+++ src/lib/large-component.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import lodash from 'lodash';
+
+	const array = [1, 2, 3];
+</script>
+
+{JSON.stringify(lodash.partition(array, (n) => n % 2))}
---
+++ src/routes/large.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import LargeComponent from '$lib/large-component.svelte';
+</script>
+
+<LargeComponent />
+
+<p><a href='/' sveltekit:prefetch>Go back home</a></p>
